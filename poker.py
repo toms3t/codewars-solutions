b0cba86e4fc21e7f4b9ae01469ccc1bf4f458b09
Tom Setliffe <tomset@outlook.com>
2017-08-22 22:05:40 -0700

    One change needed - Ace can be value of 1

--- poker.py
+++ poker.py
@@ -1,80 +1,4 @@
-# A famous casino is suddenly faced with a sharp decline of their revenues. They decide to offer Texas hold'em also
-# online. Can you help them by writing an algorithm that can rank poker hands?
-#
-# Task:
-#
-# Create a poker hand that has a method to compare itself to another poker hand:
-#     compare_with(self, other_hand)
-# A poker hand has a constructor that accepts a string containing 5 cards:
-#     PokerHand(hand)
-# The characteristics of the string of cards are:
-# A space is used as card seperator
-# Each card consists of two characters
-# The first character is the value of the card, valid characters are:
-# 2, 3, 4, 5, 6, 7, 8, 9, T(en), J(ack), Q(ueen), K(ing), A(ce)
-# The second character represents the suit, valid characters are:
-# S(pades), H(earts), D(iamonds), C(lubs)
-#
-# The result of your poker hand compare can be one of these 3 options:
-#     RESULT = ["Loss", "Tie", "Win"]
-# Apply the Texas Hold'em rules for ranking the cards.
-# There is no ranking for the suits.
-
-# '2C 3H 4D 5S QH'
-
-
-# 1. Royal Flush
-# The best hand in poker, a "royal flush" is extremely rare, consisting of the highest possible straight (ace to ten)
-# with all cards being the same suit.
-# {A-Spades}{K-Spades}{Q-Spades}{J-Spades}{10-Spades} would represent a royal flush.
-#
-# 2. Straight Flush
-# A straight flush is a five-card straight (that is, five cards of consecutive rank) with all five being the same suit.
-# (A royal flush is an example of a straight flush — the highest one.)
-# For example, {9-Hearts}{8-Hearts}{7-Hearts}{6-Hearts}{5-Hearts} is a straight flush and would beat even an ace-high
-# flush.
-#
-# 3. Four of a kind
-# Next comes four of a kind or "quads," that is, four cards of the same rank.
-# A hand like {Q-Spades}{Q-Hearts}{Q-Clubs}{Q-Diamonds}{2-Clubs} is four of a kind and would beat any hand other than
-# a straight flush or royal flush.
-#
-# 4. Full house
-# A full house consists of three cards of the same rank along with two more cards of the same rank (in other words,
-# three of a kind plus a pair). {J-Hearts}{J-Clubs}{J-Diamonds}{10-Clubs}{10-Diamonds} is an example of a full house
-# and beats a flush, a straight, and all lesser-ranked hands.
-#
-# 5. Flush
-# A flush consists of any five cards of the same suit, such as
-# {K-Diamonds}{J-Diamonds}{7-Diamonds}{5-Diamonds}{2-Diamonds}. When comparing two flushes, the one containing the
-# highest-ranked card is best. Therefore a flush containing an ace (an "ace-high flush") would beat this king-high flush
-#
-# 6. Straight
-# A straight is made from any five cards consecutive in rank that are not all the same suit, such as
-# {J-Hearts}{10-Clubs}{9-Diamonds}{8-Hearts}{7-Spades}. When comparing two straights, the one with the highest-ranking
-# card is best, so this jack-high straight would beat a ten-high straight (going from ten to six) and all lower ones.
-#
-# 7. Three of a kind
-# Making three of a kind or "trips" requires having three cards of the same rank among your five —
-# for example, {6-Hearts}{6-Clubs}{6-Diamonds}{K-Spades}{9-Clubs}. Three aces is the best possible three of a kind to
-# make, followed by three kings, three queens, and so forth.
-#
-# 8. Two pair
-# Two pair involves having two cards of the same rank plus two more cards of the same rank among the five in your hand,
-# such as {A-Clubs}{A-Diamonds}{5-Spades}{5-Hearts}{3-Clubs}. The best possible two-pair hand is aces and kings.
-#
-# 9. One pair
-# Making one pair means having two cards of the same rank in your five-card poker hand, with the other three cards being
-#  unpaired. For example, {K-Spades}{K-Hearts}{8-Spades}{7-Diamonds}{2-Clubs} would constitute a one-pair hand.
-#
-# 10. High card
-# A "high card" hand consists of five unpaired cards that make neither a straight nor a flush, such as
-# {A-Clubs}{K-Hearts}{J-Diamonds}{10-Spades}{8-Hearts}. The highest-ranked of the five cards determines its value, so
-# an "ace-high" hand (such as this example) would beat a "king-high" hand, and so forth.
-
-
 class PokerHand(object):
-    RESULT = ["Loss", "Tie", "Win"]
 
     CARDMAP = {
         '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
@@ -86,21 +10,15 @@ class PokerHand(object):
     def __init__(self, hand):
         self.hand = hand.split()
         self.straight = False
-        self.straight_val = 0
         self.one_pair = False
-        self.one_pair_val = 0
         self.two_pair = False
         self.two_pair_higher = 0
         self.two_pair_lower = 0
         self.three = False
-        self.three_val = 0
         self.four = False
-        self.four_val = 0
         self.result = 0
         self.high_card = None
-        self.high_card_val = 0
         self.flush = False
-        self.flush_val = 0
         self.full_house = False
         self.straight_flush = False
         self.royal_flush = False
@@ -108,28 +26,39 @@ class PokerHand(object):
         self.values = []
         self.single_values = []
 
-    def __str__(self):
-        return self.hand
-
     def get_card_values(self):
+        '''
+        This function takes in the hand as a list and parses out the card values, then sorts the values low to high
+        :return: Returns the numerical values of the cards in a given hand (Jack = 11, Queen = 12, King = 13, Ace = 14)
+        '''
+
         for card in self.hand:
             if card[0] in PokerHand.CARDMAP:
                 self.values.append(PokerHand.CARDMAP[card[0]])
         self.values.sort()
         return self.values
 
-    def evaluate(self):
+    def identify_hand(self):
+        '''
+        This function identifies the best hand that the player can make with 5 cards
+        :return: Does not return a value, only sets a object attribute to True if a hand (i.e. Two Pair) is identified
+        '''
+
+        i = 0
+        highest = 0
+        high_suit = ''
         self.values = self.get_card_values()
         self.single_values = sorted([x for x in self.values if self.values.count(x) == 1])
         prev = self.values[0]
-        i = 0
-        highest = 0
-        highsuit = ''
+
+        # Identifying High Card
         for card in self.hand:
             if PokerHand.CARDMAP[card[0]] > highest:
                 highest = PokerHand.CARDMAP[card[0]]
-                highsuit = card[-1]
-        self.high_card = str(PokerHand.CARDMAP_REVERSED[highest]) + highsuit
+                high_suit = card[-1]
+        self.high_card = str(PokerHand.CARDMAP_REVERSED[highest]) + high_suit
+
+        # Identifying a Straight
         self.score = self.values[-1]
         for val in self.values[1:]:
             if prev + 1 == val:
@@ -138,6 +67,8 @@ class PokerHand(object):
         if prev == self.values[-1] and i == 4:
             self.straight = True
             self.score = self.values[-1]
+
+        # Identifying matched cards (one-pair / two-pair / three-of-a-kind / four-of-a-kind)
         for val in self.values:
             if self.values.count(val) == 2:
                 self.one_pair = True
@@ -147,8 +78,6 @@ class PokerHand(object):
                 self.three = True
                 self.score = val
             elif self.values.count(val) == 4:
-                # self.one_pair = False
-                # self.three = False
                 self.four = True
                 self.score = val
         if self.values.count(self.values[1]) == 2 and self.values.count(self.values[3]) == 2:
@@ -156,16 +85,16 @@ class PokerHand(object):
             self.two_pair_higher = max(self.values[1], self.values[3])
             self.two_pair_lower = min(self.values[1], self.values[3])
             self.score = self.two_pair_higher
+
+        # Identifying a Full House
         if self.values.count(self.values[0]) == 3:
             if self.values.count(self.values[-1]) == 2:
                 self.full_house = True
-                # val = 0
-                # self.score = self.tiebreaker(val)
         elif self.values.count(self.values[0]) == 2:
             if self.values.count(self.values[-1]) == 3:
                 self.full_house = True
-                # val = 0
-                # self.score = self.tiebreaker(val)
+
+        # Identifying a Flush
         concat = ''.join(self.hand)
         if concat.count('C') == 5:
             self.flush = True
@@ -180,6 +109,7 @@ class PokerHand(object):
             self.flush = True
             self.score = self.values[-1]
 
+        # Identifying a Straight Flush or Royal Flush
         if self.straight == True and self.flush == True:
             self.straight_flush = True
             self.score = self.values[-1]
@@ -187,38 +117,48 @@ class PokerHand(object):
                 self.royal_flush = True
                 self.score = 100
 
+    def get_hand_value(self):
+        '''
+        This function assigns a value to the best hand identified by "identify hand()".
+        :return: Returns the self.result value which is used to rank the value of a hand
+        '''
         if self.royal_flush:
             self.result = 10
-            return 'RF', self.royal_flush, self.result
+            return self.result
         if self.straight_flush:
             self.result = 9
-            return 'SF', self.straight_flush, self.result
+            return self.result
         if self.four:
             self.result = 8
-            return 'FOUR', self.four, self.result
+            return self.result
         if self.full_house:
             self.result = 7
-            return 'FH', self.full_house, self.result
+            return self.result
         if self.flush:
             self.result = 6
-            return 'F', self.flush, self.result
+            return self.result
         if self.straight:
             self.result = 5
-            return 'S', self.straight, self.result
+            return self.result
         if self.three:
             self.result = 4
-            return 'THREE', self.three, self.result
+            return self.result
         if self.two_pair:
             self.result = 3
-            return 'TP', self.two_pair, self.result
+            return self.result
         if self.one_pair:
             self.result = 2
-            return 'OP', self.one_pair, self.result
+            return self.result
         if self.high_card:
             self.result = 1
-            return 'HC', self.high_card, self.result
+            return self.result
 
     def tiebreaker(self, other_hand):
+        '''
+        This function determines the winning hand (breaks tie) if both hands are deemed the same (i.e. both one pair).
+        :param other_hand: Separate object of type PokerHand class to compare with Self
+        :return: String('Win','Loss','Tie') is returned when a tiebreaker is needed (both hands have same result)
+        '''
         if self.one_pair:
             kicker = self.find_highest_kicker(other_hand)
             if 'self' in kicker:
@@ -269,14 +209,13 @@ class PokerHand(object):
             return "Loss"
         return "Tie"
 
-    def get_result(self):
-        if self.result > other_hand.result:
-            return "Win"
-        if self.result < other_hand.result:
-            return 'Loss'
-        return 'Tie'
-
     def find_highest_kicker(self, other_hand):
+        '''
+        This function determines the card in the hand that is the highest
+        kicker (card that is not part of the winning set of cards).
+        :param other_hand: Separate object of type PokerHand class to compare with Self
+        :return: Returns "self" or "other_hand" (whichever had highest kicker) and the value of the card
+        '''
         for x, y in list(zip(self.single_values, other_hand.single_values))[::-1]:
             if x > y:
                 return 'self' + str(x)
@@ -285,8 +224,16 @@ class PokerHand(object):
         return 'Tie'
 
     def compare_with(self, other_hand):
+        '''
+        This function compares one hand to another and returns "Win", "Loss", or "Tie" from the perspective of self
+        :param other_hand: Separate object of type PokerHand class to compare with Self
+        :return: String('Win', 'Loss', 'Tie')
+        '''
+        self.identify_hand()
         other_hand = PokerHand(other_hand)
-        other_result = other_hand.evaluate()
+        other_hand.identify_hand()
+        self.result = self.get_hand_value()
+        other_hand.result = other_hand.get_hand_value()
         if self.full_house:
             for val in self.values:
                 if self.values.count(val) == 3:
@@ -294,13 +241,7 @@ class PokerHand(object):
             for val in other_hand.values:
                 if other_hand.values.count(val) == 3:
                     other_hand.score = val
-                    # if self.score == other_hand.score:
-                    #     for val in self.values:
-                    #         if self.values.count(val) == 2:
-                    #             self.score = val
-                    #     for val in other_hand.values:
-                    #         if other_hand.values.count(val) == 2:
-                    #             other_hand.score = val
+
         if self.result > other_hand.result:
             return "Win"
         if self.result < other_hand.result:
@@ -322,10 +263,5 @@ class PokerHand(object):
                 if self.flush:
                     return self.tiebreaker(other_hand)
 
-
-a = PokerHand('2S 4S 6S 8H TS')
-print(a.evaluate())
-print(a.compare_with('AD KD QD JD TD'))
-
-# b = PokerHand('3H QS 3H TD TS')
-# print(b.evaluate())
+# a = PokerHand('5H 5S 5C AD TS')
+# print(a.compare_with('QH QS QH 9D TS'))
--- testcase.py
+++ testcase.py
@@ -1,190 +1,132 @@
 import unittest
 import poker
 
-
 class TestStringMethods(unittest.TestCase):
-    def test_hand(self):
-        '''
-        Testing Full House
-        '''
-        a = poker.PokerHand('QH QS TH TD TS')
-        self.assertEqual(a.evaluate(), ('FH', True, 7))
-
-    def test_hand2(self):
-        '''
-        Testing Straight
-        '''
-        a = poker.PokerHand('2H 3C 4H 5D 6S')
-        self.assertEqual(a.evaluate(), ('S', True, 5))
-
-    def test_hand3(self):
-        '''
-        Testing Royal Flush
-        '''
-        a = poker.PokerHand('QH AH KH TH JH')
-        self.assertEqual(a.evaluate(), ('RF', True, 10))
-
-    def test_hand4(self):
-        '''
-        Testing High Card
-        '''
-        a = poker.PokerHand('QH 3S 5H 7D 9S')
-        self.assertEqual(a.evaluate(), ('HC', 'QH', 1))
-
-    def test_hand5(self):
-        '''
-        Testing three of a kind
-        '''
-        a = poker.PokerHand('QH QS QH 5D TS')
-        self.assertEqual(a.evaluate(), ('THREE', True, 4))
-
-    def test_hand6(self):
-        '''
-        Testing two pair
-        '''
-        a = poker.PokerHand('QH QS 9H 9D TS')
-        self.assertEqual(a.evaluate(), ('TP', True, 3))
-
-    def test_hand7(self):
-        '''
-        Testing four of a kind
-        '''
-        a = poker.PokerHand('AH AS AC AD TS')
-        self.assertEqual(a.evaluate(), ('FOUR', True, 8))
-
-    def test_hand8(self):
-        '''
-        Testing High Card
-        '''
-        a = poker.PokerHand('2H 4S 6C 9D TS')
-        self.assertEqual(a.evaluate(), ('HC', 'TS', 1))
-
-    def test_hand9(self):
-        '''
-        Testing one pair
-        '''
-        a = poker.PokerHand('9H 9S 2C 4D TS')
-        self.assertEqual(a.evaluate(), ('OP', True, 2))
-
     def test_compare(self):
         '''
         Testing 4 aces beats two pair
         '''
-        a = poker.PokerHand('AH AS AC AD TS')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('QH QS 9H 9D TS'), 'Win')
+        hand = poker.PokerHand('AH AS AC AD TS')
+        self.assertEqual(hand.compare_with('QH QS 9H 9D TS'), 'Win')
 
     def test_compare2(self):
         '''
         Testing high card loses to two pair
         '''
-        a = poker.PokerHand('2H 4S 6C 8D TS')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('QH QS 9H 9D TS'), 'Loss')
+        hand = poker.PokerHand('2H 4S 6C 8D TS')
+        self.assertEqual(hand.compare_with('QH QS 9H 9D TS'), 'Loss')
 
     def test_compare3(self):
         '''
         Testing higher 4 of a kind wins
         '''
-        a = poker.PokerHand('AH AS AC AD TS')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('KH KS KD KC TS'), 'Win')
+        hand = poker.PokerHand('AH AS AC AD TS')
+        self.assertEqual(hand.compare_with('KH KS KD KC TS'), 'Win')
 
     def test_compare4(self):
         '''
         Testing full house beats straight
         '''
-        a = poker.PokerHand('8H 8S 8C 3D 3S')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('TH JS QH KD AS'), 'Win')
+        hand = poker.PokerHand('8H 8S 8C 3D 3S')
+        self.assertEqual(hand.compare_with('TH JS QH KD AS'), 'Win')
 
     def test_compare5(self):
         '''
         Testing full house loses to 4 of a kind
         '''
-        a = poker.PokerHand('AH AS AC 8D TS')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('QH QS QC QD TS'), 'Loss')
+        hand = poker.PokerHand('AH AS AC 8D TS')
+        self.assertEqual(hand.compare_with('QH QS QC QD TS'), 'Loss')
 
     def test_compare6(self):
         '''
         Testing royal flush tie
         '''
-        a = poker.PokerHand('AS KS QS JS TS')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('AH KH QH JH TH'), 'Tie')
+        hand = poker.PokerHand('AS KS QS JS TS')
+        self.assertEqual(hand.compare_with('AH KH QH JH TH'), 'Tie')
 
     def test_compare7(self):
         '''
         Testing higher straight wins
         '''
-        a = poker.PokerHand('3H 4S 5C 6D 7S')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('4H 5S 6C 7D 8S'), 'Loss')
+        hand = poker.PokerHand('3H 4S 5C 6D 7S')
+        self.assertEqual(hand.compare_with('4H 5S 6C 7D 8S'), 'Loss')
 
     def test_compare8(self):
         '''
         Testing straight tie
         '''
-        a = poker.PokerHand('3H 4S 5C 6D 7S')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('3H 4S 5C 6D 7S'), 'Tie')
+        hand = poker.PokerHand('3H 4S 5C 6D 7S')
+        self.assertEqual(hand.compare_with('3H 4S 5C 6D 7S'), 'Tie')
 
     def test_compare9(self):
         '''
         Testing higher pair of two pair wins
         '''
-        a = poker.PokerHand('KH KS 8C 8D TS')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('AH AS QC QD TS'), 'Loss')
+        hand = poker.PokerHand('KH KS 8C 8D TS')
+        self.assertEqual(hand.compare_with('AH AS QC QD TS'), 'Loss')
 
     def test_compare10(self):
         '''
         Testing higher 3 of a kind in a full house wins
         '''
-        a = poker.PokerHand('AH AS AC 8D 8S')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('QH QS QC TD TS'), 'Win')
+        hand = poker.PokerHand('AH AS AC 8D 8S')
+        self.assertEqual(hand.compare_with('QH QS QC TD TS'), 'Win')
 
     def test_compare11(self):
         '''
         Testing higher 3 of a kind in a full house wins
         '''
-        a = poker.PokerHand('5H 5S 5C 8D 8S')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('QH QS QC 2D 2S'), 'Loss')
+        hand = poker.PokerHand('5H 5S 5C 8D 8S')
+        self.assertEqual(hand.compare_with('QH QS QC 2D 2S'), 'Loss')
 
     def test_compare12(self):
         '''
         Testing higher card of a flush wins
         '''
-        a = poker.PokerHand('3H 5H 7H 8H TH')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('QS 4S 3S 7S TS'), 'Loss')
+        hand = poker.PokerHand('3H 5H 7H 8H TH')
+        self.assertEqual(hand.compare_with('QS 4S 3S 7S TS'), 'Loss')
 
     def test_compare13(self):
         '''
-        Testing higher card of a 4 of a kind wins
+        Testing higher card of hand 4 of a kind wins
         '''
-        a = poker.PokerHand('8H 8S 8C 8D TS')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('7H 7S 7C 7D TS'), 'Win')
+        hand = poker.PokerHand('8H 8S 8C 8D TS')
+        self.assertEqual(hand.compare_with('7H 7S 7C 7D TS'), 'Win')
 
     def test_compare14(self):
         '''
         Testing one pair beats high card
         '''
-        a = poker.PokerHand('8H 8S 4C 5D TS')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('3H 5S 7C 9D AS'), 'Win')
+        hand = poker.PokerHand('8H 8S 4C 5D TS')
+        self.assertEqual(hand.compare_with('3H 5S 7C 9D AS'), 'Win')
 
     def test_compare15(self):
         '''
         Testing higher card wins
         '''
-        a = poker.PokerHand('8H 5D 4C 2D TS')
-        aval = a.evaluate()
-        self.assertEqual(a.compare_with('3H 5S 7C 9D AS'), 'Loss')
+        hand = poker.PokerHand('8H 5D 4C 2D TS')
+        self.assertEqual(hand.compare_with('3H 5S 7C 9D AS'), 'Loss')
+
+    def test_compare16(self):
+        '''
+        Testing two pair tie
+        '''
+        hand = poker.PokerHand('8H 8D 4C 4D TS')
+        self.assertEqual(hand.compare_with('8S 8C 4S 4H TD'), 'Tie')
+
+    def test_compare17(self):
+        '''
+        Testing one pair tie
+        '''
+        hand = poker.PokerHand('8H 8D 5C 4D TS')
+        self.assertEqual(hand.compare_with('8S 8C 5S 4H TD'), 'Tie')
+
+    def test_compare18(self):
+        '''
+        Testing high card tie
+        '''
+        hand = poker.PokerHand('2H 4D 6C 8D TS')
+        self.assertEqual(hand.compare_with('2S 4C 6S 8H TD'), 'Tie')
 
 if __name__ == '__main__':
     unittest.main()
