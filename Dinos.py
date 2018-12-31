ae5fca4a13d01b7901073d8f18dd9f431760223c
Tom Setliffe <10659165+toms3t@users.noreply.github.com>
2018-12-31 11:55:58 -0800

    Update Dinos.py

--- Dinos.py
+++ Dinos.py
@@ -43,17 +43,6 @@ class Dino():
         # v[0] =
         return cls(name,leg_length,diet,stride_length,stance)
 
-    # @classmethod
-    # def constructor(cls):
-    #     dinospeed = {}
-    #     for k,v in Dino.pull_data().items():
-    #         k = Dino(k,v[0],v[1],v[2],v[3])
-    #         k.speed = Dino.speed_calc(k)
-    #         if k.stance == 'bipedal':
-    #             dinospeed[k.name] = k.speed
-    #     for k,v in sorted(dinospeed.items(),key=lambda x:x[1],reverse=True):
-    #         print (k)
-
 dinos = Dino.pull_data()
 
 for k,v in dinos.items():
